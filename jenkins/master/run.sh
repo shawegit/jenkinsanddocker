51fcd4db616cb7599778c141614dafa877d6c488
Simon Hawe <sh@gi.ai>
2016-12-05 21:49:36 +0100

    Update real jenkins config

--- jenkins/master/run.sh
+++ jenkins/master/run.sh
@@ -10,4 +10,5 @@ groupadd -for -g ${DOCKER_GID} ${DOCKER_GROUP}
 usermod -aG ${DOCKER_GROUP} ${JENKINS_USER}
 fi
 
+exec sudo /opt/sonar/bin/linux-x86-64/sonar.sh start &
 exec sudo -E -H -u jenkins bash -c /usr/local/bin/jenkins.sh
